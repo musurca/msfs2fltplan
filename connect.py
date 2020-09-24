d846132dd8f29afd162e85bc093320717da99786
@musurca <nick.musurca@gmail.com>
2020-09-23 20:49:31 -0700

    handle exceptions, refactor

--- connect.py
+++ connect.py
@@ -31,28 +31,46 @@ LOGGER.info("START")
 sm = SimConnect()
 aq = AircraftRequests(sm)
 
-# Make the UDP socket to communicate with FltPlan Go
-fpgSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Create the UDP socket to communicate with FltPlan Go
+try:
+    fpgSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error as msg:
+    print(msg)
+    sys.exit()
 
 def sendToFltplan(msg):
-    #print(msg)
-    fpgSock.sendto(bytes(msg,'utf-8'), FLTPLAN_ADDR)
+    try:
+        fpgSock.sendto(bytes(msg,'utf-8'), FLTPLAN_ADDR)
+    except socket.error as msg:
+        print(msg)
+        sys.exit()
 
 def numFormat(num):
     return str(round(num,5))
 
+def getSimvar(key):
+    return aq.PositionandSpeedData.get(key)
+
 while not sm.quit:
-    lat, lon, alt, hdg, spd, pitch, bank = aq.PositionandSpeedData.get('PLANE_LATITUDE'), aq.PositionandSpeedData.get('PLANE_LONGITUDE'), aq.PositionandSpeedData.get('PLANE_ALTITUDE'), aq.PositionandSpeedData.get('PLANE_HEADING_DEGREES_TRUE'), aq.PositionandSpeedData.get('GROUND_VELOCITY'), aq.PositionandSpeedData.get('PLANE_PITCH_DEGREES'), aq.PositionandSpeedData.get('PLANE_BANK_DEGREES')
+    # Get crucial simvars via SimConnect
+    lat = getSimvar('PLANE_LATITUDE')
+    lon = getSimvar('PLANE_LONGITUDE')
+    alt = getSimvar('PLANE_ALTITUDE')
+    hdg = getSimvar('PLANE_HEADING_DEGREES_TRUE')
+    spd = getSimvar('GROUND_VELOCITY')
+    pitch = getSimvar('PLANE_PITCH_DEGREES')
+    bank = getSimvar('PLANE_BANK_DEGREES')
     
+    # Perform unit conversions to X-Plane standard
     alt = alt * 0.3048      # feet -> meters
-    hdg = degrees(hdg)
+    hdg = degrees(hdg)      # radians -> degrees
     spd = spd / 1.945       # knots -> m/s
     pitch = degrees(pitch)
     bank = degrees(bank)
 
+    # Send pseudo-NMEA sentences masquerading as X-Plane
     #b'XGPS1,-73.878280,40.782172,2.2004,122.2988,0.0692'
     gpsMsg = "XGPS1," + numFormat(lon) + "," + numFormat(lat) + "," + numFormat(alt) + "," + numFormat(hdg) + "," + numFormat(spd)
-    
     #b'XATT1,122.3,-6.1,0.3,-0.0014,0.0629,-0.0003,-0.0,0.1,-0.0,-0.02,1.86,0.21'
     attMsg = "XATT1," + numFormat(hdg) + "," + numFormat(pitch) + "," + numFormat(bank) + ",0,0,0,0,0,0,0,0,0"
     
